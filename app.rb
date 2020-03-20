79b98cecc3b740fcbc6b06ee0cbf4168f8289433
jasper-kellogg <malte.jasper@kellogg.northwestern.edu>
2020-03-20 14:33:57 +0000

    Ready for Heroku

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ require "sinatra/reloader" if development?                                      
 require "sequel"                                                                      #
 require "logger"                                                                      #
 require "twilio-ruby"                                                                 #
-require "bcrypt"                                                                      #
+require "bcrypt"    
+require "geocoder"                                                                  #
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
 DB ||= Sequel.connect(connection_string)                                              #
 DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
@@ -16,3 +17,81 @@ after { puts; }                                                                 
 
 events_table = DB.from(:events)
 rsvps_table = DB.from(:rsvps)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+get "/" do
+    puts events_table.all
+    @events = events_table.all.to_a
+    view "events"
+end
+
+get "/events/new" do
+    view "new_event"
+end
+
+get "/events/:id" do
+    @event = events_table.where(id: params[:id]).to_a[0]
+    @rsvps = rsvps_table.where(event_id: @event[:id])
+    @going_count = rsvps_table.where(event_id: @event[:id], going: true).count
+    @users_table = users_table
+    view "event"
+end
+
+get "/events/:id/rsvps/new" do
+    @event = events_table.where(id: params[:id]).to_a[0]
+    view "new_rsvp"
+end
+
+get "/events/:id/rsvps/create" do
+    puts params
+    @event = events_table.where(id: params["id"]).to_a[0]
+    rsvps_table.insert(event_id: params["id"],
+                       user_id: session["user_id"],
+                       going: params["going"],)
+    view "create_rsvp"
+end
+
+get "/users/new" do
+    view "new_user"
+end
+
+post "/users/create" do
+    puts params
+    hashed_password = BCrypt::Password.create(params["password"])
+    users_table.insert(name: params["name"], email: params["email"], password: hashed_password)
+    view "create_user"
+end
+
+post "/events/create" do
+    puts params
+    results = Geocoder.search(params["location"])
+    lat_long = results.first.coordinates
+    puts lat_long
+    events_table.insert(title: params["title"], date: params["date"], location: params["location"], lat: lat_long[0], long: lat_long[1])
+    view "create_event"
+end
+
+get "/logins/new" do
+    view "new_login"
+end
+
+post "/logins/create" do
+    user = users_table.where(email: params["email"]).to_a[0]
+    if user && BCrypt::Password::new(user[:password]) == params["password"]
+        session["user_id"] = user[:id]
+        @current_user = user
+        view "create_login"
+    else
+        view "create_login_failed"
+    end
+end
+
+get "/logout" do
+    session["user_id"] = nil
+    @current_user = nil
+    view "logout"
+end
--- createdb.rb
+++ createdb.rb
@@ -8,28 +8,44 @@ DB = Sequel.connect(connection_string)                                          
 DB.create_table! :events do
   primary_key :id
   String :title
-  String :description, text: true
   String :date
   String :location
+  String :lat
+  String :long
+
 end
 DB.create_table! :rsvps do
   primary_key :id
   foreign_key :event_id
+  foreign_key :user_id
   Boolean :going
+
+
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
 events_table = DB.from(:events)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+events_table.insert(title: "Grocery shopping", 
+                    date: "Monday, 23th March",
+                    location: "Kellogg Global Hub",
+                    lat: "42.0574388",
+                    long: "-87.6721363")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+events_table.insert(title: "Help in the garden", 
+                    date: "Sunday, 22nd March",
+                    location: "E2 Evanston",
+                    lat: "42.04840850830078",
+                    long: "-87.68678283691406")
+
+events_table.insert(title: "Lift to the doctor", 
+                    date: "Tuesday, 24th March",
+                    location: "Evanston",
+                    lat: "42.065773010253906",
+                    long: "-87.68450164794922")
